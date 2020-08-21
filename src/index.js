4dfa62f7471d8990ed923a57be5e62b4781f6c4f
tchiteu <matheussantos.contato362@gmail.com>
2020-08-21 09:24:48 +0000

    Ajustando SSL do servidor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 require("dotenv-safe").config();
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
@@ -11,7 +14,25 @@ app.use(routes);
 
 const port = process.env.PORT || 3000;
 
-console.info(`[SERVIDOR]: Rodando na porta ${port}`);
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/api.moment.works/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/api.moment.works/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/api.moment.works/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
 process.on('SIGINT', () => { console.info("Até mais!"); process.exit(); });
 
-app.listen(process.env.PORT || 3000);
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
